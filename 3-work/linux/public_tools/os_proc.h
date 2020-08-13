2e3ed87f77d7e193bc2dca7bb6db09f4fc0c690f
cj <593184971@qq.com>
2020-08-13 17:07:59 +0800

    ..

--- 3-work/linux/public_tools/os_proc.h
+++ 3-work/linux/public_tools/os_proc.h
@@ -40,24 +40,29 @@ struct proc_info
 	unsigned pgid;
 	unsigned sid;
 	int tty;
-	unsigned long utime;
-	unsigned long stime;
+	unsigned long utime; //该任务在用户态运行的时间，单位为jiffies
+	unsigned long stime; //该任务在核心态运行的时间，单位为jiffies
 	long tasknice; //任务的静态优先级。
-	unsigned long start_time; //该任务启动的时间，单位为 jiffies。
+	unsigned long start_time; //该任务启动的时间，单位为 jiffies, (系统运行时间 + start_time/HZ 单位s)
 	unsigned long vsz; //该任务的虚拟地址空间大小
 	unsigned long rss; //该任务当前驻留物理地址空间的大小
 	long VmRSS;
 	unsigned long ticks; //utime+stime
 
-	unsigned long use_mem;
-	unsigned long use_cpu;
+	unsigned long use_mem; //内存使用率
+	unsigned long use_cpu; //cpu使用率
 };
 
 void proc_scan(struct list_head *plist);
 void proc_destroy(struct list_head *plist);
+
+int os_proc_exists(int pid, const char *comm, const char *args);
+
+int os_proc_find_pid(const char *comm, const char *args);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* SRC_SRC_OS_PROC_H_ */
+
