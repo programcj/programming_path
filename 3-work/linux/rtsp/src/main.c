5ab657e29c789cbad8e7ed95ae0e3aa124d03b40
cj <593184971@qq.com>
2020-10-30 18:16:27 +0800

    rtsp

---
+++ 3-work/linux/rtsp/src/main.c
@@ -0,0 +1,404 @@
+/*
+ * main.c
+ *
+ *  Created on: 2020年10月30日
+ *      Author: cc
+ *
+ *                .-~~~~~~~~~-._       _.-~~~~~~~~~-.
+ *            __.'              ~.   .~              `.__
+ *          .'//                  \./                  \\`.
+ *        .'//                     |                     \\`.
+ *      .'// .-~"""""""~~~~-._     |     _,-~~~~"""""""~-. \\`.
+ *    .'//.-"                 `-.  |  .-'                 "-.\\`.
+ *  .'//______.============-..   \ | /   ..-============.______\\`.
+ *.'______________________________\|/______________________________`.
+ *.'_________________________________________________________________`.
+ * 
+ * 请注意编码格式
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "base64.h"
+#include <poll.h>
+#include <errno.h>
+
+#define DEBUG 1
+
+int string_http_head_get_content_length(const char *buffhead, int headlen)
+{
+	const char *ptr = buffhead;
+	const char *ptr_end = buffhead + headlen;
+	int content_length = -1;
+	while (ptr < ptr_end)
+	{
+		if (0 == strncmp(ptr, "Content-Length:", strlen("Content-Length:")))
+		{
+			content_length = atoi(ptr + strlen("Content-Length:"));
+		}
+		ptr = strchr(ptr, '\n');
+		if (!ptr)
+			break;
+		if (ptr + 2 < ptr_end)
+		{
+			if (ptr[1] == '\r' && ptr[2] == '\n')
+				break;
+		}
+		ptr++;
+	}
+	return content_length;
+}
+
+int string_http_heard_getlength(const char *buf, int len)
+{
+	const char *end = buf + len;
+	const char *ptr = buf;
+
+	while (ptr < end)
+	{
+		if (ptr + 1 < end && *ptr == '\n' && *(ptr + 1) == '\n')
+		{
+			return ptr - buf + 2;
+		}
+		if (ptr + 2 < end && *ptr == '\n' && *(ptr + 1) == '\r' && *(ptr + 2) == '\n')
+		{
+			return ptr - buf + 3;
+		}
+		if (ptr + 2 < end && *ptr == '\r' && *(ptr + 1) == '\n' && *(ptr + 2) == '\n')
+		{
+			return ptr - buf + 3;
+		}
+		if (ptr + 3 < end && *ptr == '\r' && *(ptr + 1) == '\n' && *(ptr + 2) == '\r' && *(ptr + 3) == '\n')
+		{
+			return ptr - buf + 4;
+		}
+		ptr++;
+	}
+	return -1;
+}
+
+struct URLInfo
+{
+	char prefix[10];
+	char host[30];
+	int port;
+	char useranme[30];
+	char password[30];
+	const char *path;
+
+	char Authorization[100];
+};
+
+typedef struct _RTSPClient
+{
+	char *url;
+	int fd;
+	struct URLInfo urlinfo;
+} RTSPClient;
+
+int urldecode(const char *url, struct URLInfo *urlinfo)
+{
+	const char *ptr = url, *pos;
+	const char *pend;
+	int len = strlen(url);
+	int flagauth = 0;
+
+	ptr = strstr(url, "://");
+	if (ptr == NULL)
+		return -1;
+	ptr += 3;
+	strncpy(urlinfo->prefix, url, ptr - url);
+
+	pend = strchr(ptr, '/');
+	if (pend == NULL)
+		pend = url + len;
+	//decode username password
+	for (pos = ptr; pos < pend && *pos != '/'; pos++)
+	{
+		if (*pos == '@' && flagauth == 0)
+		{
+			flagauth = 1;
+			if (pos - ptr > sizeof(urlinfo->useranme))
+				printf("[%s]username length > %ld\n", url, sizeof(urlinfo->useranme));
+			else
+				strncpy(urlinfo->useranme, ptr, pos - ptr);
+			ptr = pos + 1;
+		}
+
+		if (flagauth && *pos == ':')
+		{
+			if (pos - ptr > sizeof(urlinfo->password))
+				printf("[%s]password length > %ld\n", url, sizeof(urlinfo->password));
+			else
+				strncpy(urlinfo->password, ptr, pos - ptr);
+			pos++;
+			ptr = pos;
+			break;
+		}
+	}
+	//
+	if (ptr >= pend)
+	{
+		printf("[%s]unknow host port\n", url);
+		return -1;
+	}
+	//host and port;
+	for (pos = ptr; pos < pend; pos++)
+		if (*pos == ':')
+			break;
+	strncpy(urlinfo->host, ptr, pos - ptr);
+	if (*pos == ':')
+		ptr = pos + 1;
+	else
+		ptr = pos;
+
+	if (ptr < pend)
+	{
+		urlinfo->port = atoi(ptr);
+		ptr = pend;
+	}
+	else
+	{
+		if (strcmp(urlinfo->prefix, "http://") == 0)
+			urlinfo->port = 80;
+		if (strcmp(urlinfo->prefix, "https://") == 0)
+			urlinfo->port = 443;
+	}
+	urlinfo->path = ptr;
+
+#if DEBUG
+	printf("prefix:[%s]\n", urlinfo->prefix);
+	printf("useranme:[%s]\n", urlinfo->useranme);
+	printf("password:[%s]\n", urlinfo->password);
+	printf("host:[%s]\n", urlinfo->host);
+	printf("port:[%d]\n", urlinfo->port);
+	printf("path %s\n", urlinfo->path);
+#endif
+	return 0;
+}
+
+int socket_tcp(const char *host, int port)
+{
+	int ret;
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in in;
+
+	memset(&in, 0, sizeof(in));
+	in.sin_family = AF_INET;
+	in.sin_port = htons(port);
+	ret = inet_pton(AF_INET, host, &in.sin_addr.s_addr);
+	if (ret < 0)
+	{
+		fprintf(stderr, "host not ipaddress [%s]\n", host);
+		goto _err;
+	}
+
+	ret = connect(fd, (struct sockaddr *)&in, sizeof(in));
+	if (ret != 0)
+	{
+		fprintf(stderr, "not connect ot host [%s]\n", host);
+		goto _err;
+	}
+	return fd;
+
+_err:
+	if (fd != -1)
+		close(fd);
+	return -1;
+}
+
+int socket_send_text(int fd, const char *buf)
+{
+	return send(fd, buf, strlen(buf), 0);
+}
+
+int socket_recv_text(int fd, char *buf, int len)
+{
+	return recv(fd, buf, len, 0);
+}
+
+int rtsp_response_recv(int fd, char **text, size_t *textlen)
+{
+	struct pollfd pofd;
+	int ret;
+
+	memset(&pofd, 0, sizeof(pofd));
+	pofd.fd = fd;
+	pofd.events = POLLIN;
+
+	char *membuf = NULL;
+	size_t memlen = 0;
+
+	FILE *fp = open_memstream(&membuf, &memlen);
+	char buff[1024];
+	int content_length = 0;
+	int head_lenght = -1;
+
+	while (1)
+	{
+		ret = poll(&pofd, 1, 100);
+		if (ret == 0)
+			continue;
+		if (ret > 0)
+		{
+			if (POLLIN == (pofd.revents & POLLIN))
+			{
+				ret = recv(fd, buff, sizeof(buff), 0);
+				if (ret <= 0)
+				{
+					if (ret < 0 && (errno == EAGAIN || errno == EINTR))
+						continue;
+					break;
+				}
+				if (ret > 0)
+				{
+					fwrite(buff, ret, 1, fp);
+					fflush(fp);
+					//printf("membuf:%s, %d\n", membuf, memlen);
+				}
+
+				if (head_lenght == -1)
+				{
+					head_lenght = string_http_heard_getlength(membuf, memlen);
+					if (head_lenght != -1)
+						content_length = string_http_head_get_content_length(membuf, head_lenght);
+					if (content_length == -1)
+						content_length = 0;
+				}
+
+				if (head_lenght != -1)
+				{
+					if (content_length + head_lenght == memlen)
+						break;
+				}
+			}
+		}
+	}
+	fclose(fp);
+	if (memlen == 0)
+	{
+		if (membuf != NULL)
+		{
+			free(membuf);
+			membuf = NULL;
+		}
+	}
+
+	*text = membuf;
+	*textlen = memlen;
+
+#if DEBUG
+	printf("================\n%s\n=============\n", membuf);
+#endif
+	return 0;
+}
+
+int rtsp_DESCRIBE_base(int fd, struct URLInfo *urlinfo, char *authstr)
+{
+	char url[100];
+	snprintf(url, sizeof(url), "DESCRIBE %s%s:%d%s RTSP/1.0\r\n", urlinfo->prefix, urlinfo->host, urlinfo->port, urlinfo->path);
+
+	socket_send_text(fd, url);
+	socket_send_text(fd, "CSeq: 1\r\n");
+	socket_send_text(fd, "Accept: application/sdp\r\n");
+	if (strlen(urlinfo->Authorization) > 0)
+		socket_send_text(fd, urlinfo->Authorization);
+	socket_send_text(fd, "User-Agent: RTSPClient1.0\r\n");
+	socket_send_text(fd, "\r\n");
+
+	char *resp_text = NULL;
+	size_t resp_len = 0;
+	int ret = rtsp_response_recv(fd, &resp_text, &resp_len);
+
+	char pref[10];
+	int code = 0;
+
+	if (resp_text)
+		sscanf(resp_text, "%[^ ] %d ", pref, &code);
+	else
+		code = -1;
+	if (code == 401)
+	{
+		//需要加密发送哦
+		const char *auth = strstr(resp_text, "WWW-Authenticate:");
+		if (auth)
+		{
+			auth += strlen("WWW-Authenticate:");
+			if (*auth == ' ')
+				auth++;
+			char *next = strchr(auth, '\r');
+			if (next)
+				*next = 0;
+			strcpy(authstr, auth);
+		}
+	}
+
+	if (code == 200)
+	{
+		//需要得到 sdp内容
+		int head_len = string_http_heard_getlength(resp_text, strlen(resp_text));
+		printf("SDP数据:%s", resp_text + head_len);
+		//m: media descriptions
+		//a: attributes
+	}
+	if (resp_text)
+		free(resp_text);
+	return code;
+}
+
+int rtsp_DESCRIBE(int fd, struct URLInfo *urlinfo)
+{
+	int ret;
+	char authstr[300];
+	ret = rtsp_DESCRIBE_base(fd, urlinfo, authstr);
+	if (ret == 401)
+	{
+		printf("auth:%s\n", authstr);
+		if (strncmp(authstr, "Basic ", 6) == 0)
+		{
+			char pwd[100];
+			char base64str[200];
+			snprintf(pwd, sizeof(pwd), "%s:%s", urlinfo->useranme, urlinfo->password);
+			base64_encode(pwd, strlen(pwd), base64str);
+			sprintf(urlinfo->Authorization, "Authorization: Basic %s\r\n", base64str);
+		}
+
+		ret = rtsp_DESCRIBE_base(fd, urlinfo, authstr);
+		if (ret == 401)
+			return 401;
+	}
+	return ret;
+}
+
+RTSPClient *RTSPClient_open(const char *url)
+{
+	RTSPClient *client = (RTSPClient *)calloc(sizeof(RTSPClient), 1);
+	if (client == NULL)
+		return NULL;
+	client->url = strdup(url);
+	urldecode(client->url, &client->urlinfo);
+
+	int fd = socket_tcp(client->urlinfo.host, client->urlinfo.port);
+	if (fd == -1)
+	{
+
+		return NULL;
+	}
+	printf("connect success %d,[%s]\n", fd, client->urlinfo.host);
+	client->fd = fd;
+	rtsp_DESCRIBE(fd, &client->urlinfo);
+	return client;
+}
+
+int main(int argc, char **argv)
+{
+	const char *url = "rtsp://admin@admin:192.168.0.150:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif";
+	RTSPClient_open(url);
+
+	return 0;
+}
